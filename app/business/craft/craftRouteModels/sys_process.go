e85f24554fdb7c528ea36408e042203839ae8e69
zhanglei <357733652@qq.com>
2025-05-31 12:51:02 +0800

    feat: fix cmd position

--- app/business/craft/craftRouteModels/sys_process.go
+++ app/business/craft/craftRouteModels/sys_process.go
@@ -6,17 +6,17 @@ import (
 
 // SysProProcess 生产工序表
 type SysProProcess struct {
-	ProcessID      int64  `gorm:"column:process_id;primaryKey;autoIncrement:true;comment:工序ID" json:"process_id"`   // 工序ID
-	ProcessCode    string `gorm:"column:process_code;not null;comment:工序编码" json:"process_code" binding:"required"` // 工序编码
-	ProcessName    string `gorm:"column:process_name;not null;comment:工序名称" json:"process_name" binding:"required"` // 工序名称
-	Attention      string `gorm:"column:attention;comment:工艺要求" json:"attention"`                                   // 工艺要求
-	Remark         string `gorm:"column:remark;comment:备注" json:"remark"`                                           // 备注
-	Attr1          string `gorm:"column:attr1;comment:预留字段1" json:"attr1"`                                          // 预留字段1
-	Attr2          string `gorm:"column:attr2;comment:预留字段2" json:"attr2"`                                          // 预留字段2
-	Attr3          int32  `gorm:"column:attr3;comment:预留字段3" json:"attr3"`                                          // 预留字段3
-	Attr4          int32  `gorm:"column:attr4;comment:预留字段4" json:"attr4"`                                          // 预留字段4
-	Status         bool   `gorm:"column:status;comment:是否启用（0禁用 1启用）" json:"status"`                                // 是否启用（0禁用 1启用）
-	State          bool   `gorm:"column:state;comment:操作状态（0正常 -1删除）" json:"state"`                                 // 操作状态（0正常 -1删除）
+	ProcessID      int64  `gorm:"column:process_id;primaryKey;autoIncrement:true;comment:工序ID" json:"process_id,string"` // 工序ID
+	ProcessCode    string `gorm:"column:process_code;not null;comment:工序编码" json:"process_code" binding:"required"`      // 工序编码
+	ProcessName    string `gorm:"column:process_name;not null;comment:工序名称" json:"process_name" binding:"required"`      // 工序名称
+	Attention      string `gorm:"column:attention;comment:工艺要求" json:"attention"`                                        // 工艺要求
+	Remark         string `gorm:"column:remark;comment:备注" json:"remark"`                                                // 备注
+	Attr1          string `gorm:"column:attr1;comment:预留字段1" json:"attr1"`                                               // 预留字段1
+	Attr2          string `gorm:"column:attr2;comment:预留字段2" json:"attr2"`                                               // 预留字段2
+	Attr3          int32  `gorm:"column:attr3;comment:预留字段3" json:"attr3"`                                               // 预留字段3
+	Attr4          int32  `gorm:"column:attr4;comment:预留字段4" json:"attr4"`                                               // 预留字段4
+	Status         bool   `gorm:"column:status;comment:是否启用（0禁用 1启用）" json:"status"`                                     // 是否启用（0禁用 1启用）
+	State          bool   `gorm:"column:state;comment:操作状态（0正常 -1删除）" json:"state"`                                      // 操作状态（0正常 -1删除）
 	CreateUserName string `json:"createUserName" gorm:"-"`
 	UpdateUserName string `json:"updateUserName" gorm:"-"`
 	baize.BaseEntity
@@ -38,16 +38,16 @@ func NewSysProProcess(req *SysProSetProcessReq) *SysProProcess {
 }
 
 type SysProSetProcessReq struct {
-	ProcessID   int64  `gorm:"column:process_id;primaryKey;autoIncrement:true;comment:工序ID" json:"process_id"`   // 工序ID
-	ProcessCode string `gorm:"column:process_code;not null;comment:工序编码" json:"process_code" binding:"required"` // 工序编码
-	ProcessName string `gorm:"column:process_name;not null;comment:工序名称" json:"process_name" binding:"required"` // 工序名称
-	Attention   string `gorm:"column:attention;comment:工艺要求" json:"attention"`                                   // 工艺要求
-	Remark      string `gorm:"column:remark;comment:备注" json:"remark"`                                           // 备注
-	Attr1       string `gorm:"column:attr1;comment:预留字段1" json:"attr1"`                                          // 预留字段1
-	Attr2       string `gorm:"column:attr2;comment:预留字段2" json:"attr2"`                                          // 预留字段2
-	Attr3       int32  `gorm:"column:attr3;comment:预留字段3" json:"attr3"`                                          // 预留字段3
-	Attr4       int32  `gorm:"column:attr4;comment:预留字段4" json:"attr4"`                                          // 预留字段4
-	Status      bool   `gorm:"column:status;comment:是否启用（0禁用 1启用）" json:"status"`                                // 是否启用（0禁用 1启用）
+	ProcessID   int64  `gorm:"column:process_id;primaryKey;autoIncrement:true;comment:工序ID" json:"process_id,string"` // 工序ID
+	ProcessCode string `gorm:"column:process_code;not null;comment:工序编码" json:"process_code" binding:"required"`      // 工序编码
+	ProcessName string `gorm:"column:process_name;not null;comment:工序名称" json:"process_name" binding:"required"`      // 工序名称
+	Attention   string `gorm:"column:attention;comment:工艺要求" json:"attention"`                                        // 工艺要求
+	Remark      string `gorm:"column:remark;comment:备注" json:"remark"`                                                // 备注
+	Attr1       string `gorm:"column:attr1;comment:预留字段1" json:"attr1"`                                               // 预留字段1
+	Attr2       string `gorm:"column:attr2;comment:预留字段2" json:"attr2"`                                               // 预留字段2
+	Attr3       int32  `gorm:"column:attr3;comment:预留字段3" json:"attr3"`                                               // 预留字段3
+	Attr4       int32  `gorm:"column:attr4;comment:预留字段4" json:"attr4"`                                               // 预留字段4
+	Status      bool   `gorm:"column:status;comment:是否启用（0禁用 1启用）" json:"status"`                                     // 是否启用（0禁用 1启用）
 }
 
 type SysProProcessListReq struct {
