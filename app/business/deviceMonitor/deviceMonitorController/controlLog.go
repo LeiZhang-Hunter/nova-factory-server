85a9d9f104554b69c85674a50f4ef0467119c83a
zhanglei <357733652@qq.com>
2025-11-15 22:41:51 +0800

    feat: 智能控制日志加入时间搜索

--- app/business/deviceMonitor/deviceMonitorController/controlLog.go
+++ app/business/deviceMonitor/deviceMonitorController/controlLog.go
@@ -23,6 +23,13 @@ func (that *ControlLog) PrivateRoutes(router *gin.RouterGroup) {
 	group.GET("/list", middlewares.HasPermission("metric:monitor:control:log:list"), that.List)
 }
 
+// List 控制记录列表
+// @Summary 控制记录列表
+// @Description 控制记录列表
+// @Param  object query deviceMonitorModel.ControlLogListReq true "控制记录列表"
+// @Tags 设备接入/控制记录
+// @Success 200 {object}  response.ResponseData "获取成功"
+// @Router /device/monitor/control/log/list [get]
 func (that *ControlLog) List(c *gin.Context) {
 	req := new(deviceMonitorModel.ControlLogListReq)
 	err := c.ShouldBindQuery(req)
--- app/business/metric/device/metricDao/metricDaoIMpl/iControlLogDaoImpl.go
+++ app/business/metric/device/metricDao/metricDaoIMpl/iControlLogDaoImpl.go
@@ -8,6 +8,8 @@ import (
 	"nova-factory-server/app/business/metric/device/metricDao"
 	"nova-factory-server/app/business/metric/device/metricModels"
 	"nova-factory-server/app/datasource/clickhouse"
+	"nova-factory-server/app/utils/time"
+	systime "time"
 )
 
 type IControlLogDaoImpl struct {
@@ -36,6 +38,9 @@ func (i *IControlLogDaoImpl) Export(ctx context.Context, data []*metricModels.No
 
 // List 控制列表
 func (i *IControlLogDaoImpl) List(c *gin.Context, req *deviceMonitorModel.ControlLogListReq) (*metricModels.NovaControlLogList, error) {
+	if req == nil {
+		req = &deviceMonitorModel.ControlLogListReq{}
+	}
 	db := i.clickhouse.DB().Table(i.tableName)
 
 	if req != nil && req.DeviceID != 0 {
@@ -44,6 +49,20 @@ func (i *IControlLogDaoImpl) List(c *gin.Context, req *deviceMonitorModel.Contro
 	if req != nil && req.DataId != 0 {
 		db = db.Where("data_id = ?", req.DataId)
 	}
+
+	var startTime string
+	if req.Start > 0 {
+		startTime = time.GetStartTime(req.Start, 200)
+	} else {
+		start := systime.Now().UnixMilli() - 86400*1000
+		startTime = time.GetStartTime(uint64(start), 200)
+	}
+	endTime := time.GetEndTimeUseNow(req.End, true)
+	if req.DeviceID != 0 && req.DataId != 0 && startTime != "" && endTime != "" {
+		db = db.Where("time_unix > ?", startTime)
+		db = db.Where("time_unix <= ?", endTime)
+	}
+
 	size := 0
 	if req == nil || req.Size <= 0 {
 		size = 20
@@ -67,7 +86,7 @@ func (i *IControlLogDaoImpl) List(c *gin.Context, req *deviceMonitorModel.Contro
 		}, ret.Error
 	}
 
-	ret = db.Offset(offset).Order("start_time_unix desc").Limit(size).Find(&dto)
+	ret = db.Debug().Offset(offset).Order("start_time_unix desc").Limit(size).Find(&dto)
 	if ret.Error != nil {
 		return &metricModels.NovaControlLogList{
 			Rows:  make([]*metricModels.NovaControlLog, 0),
