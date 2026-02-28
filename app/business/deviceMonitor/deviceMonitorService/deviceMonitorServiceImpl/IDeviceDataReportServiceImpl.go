fbd040e94bdbc5d079a7931e20f458e378f870fb
zhanglei <357733652@qq.com>
2026-03-01 01:17:44 +0800

    fix: 设备测点选择

--- app/business/deviceMonitor/deviceMonitorService/deviceMonitorServiceImpl/IDeviceDataReportServiceImpl.go
+++ app/business/deviceMonitor/deviceMonitorService/deviceMonitorServiceImpl/IDeviceDataReportServiceImpl.go
@@ -1,13 +1,14 @@
 package deviceMonitorServiceImpl
 
 import (
-	"github.com/gin-gonic/gin"
-	"go.uber.org/zap"
 	"nova-factory-server/app/business/asset/device/deviceDao"
 	"nova-factory-server/app/business/asset/device/deviceModels"
 	"nova-factory-server/app/business/deviceMonitor/deviceMonitorDao"
 	"nova-factory-server/app/business/deviceMonitor/deviceMonitorModel"
 	"nova-factory-server/app/business/deviceMonitor/deviceMonitorService"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type IDeviceDataReportServiceImpl struct {
@@ -26,6 +27,45 @@ func (i *IDeviceDataReportServiceImpl) DevList(c *gin.Context) ([]deviceMonitorM
 	list, err := i.dao.DevList(c)
 	if err != nil {
 		return nil, err
+	}
+	if len(list) == 0 {
+		return list, nil
+	}
+
+	deviceIdMap := make(map[int64]int)
+	deviceIds := make([]int64, 0)
+	for _, v := range list {
+		deviceIdMap[v.DeviceID] = 0
+	}
+
+	for id := range deviceIdMap {
+		if id != 0 {
+			deviceIds = append(deviceIds, id)
+		}
+	}
+
+	deviceList, err := i.deviceDao.GetByIds(c, deviceIds)
+	if err != nil {
+		zap.L().Error("get dev list error", zap.Error(err))
+	}
+
+	if len(deviceList) == 0 {
+		return list, nil
+	}
+	devMap := make(map[uint64]*deviceModels.DeviceVO, 0)
+	for _, v := range deviceList {
+		devMap[(v.DeviceId)] = v
+	}
+
+	for k, v := range list {
+		deviceInfo, ok := devMap[uint64(v.DeviceID)]
+		if !ok {
+			continue
+		}
+		if deviceInfo.Name == nil {
+			continue
+		}
+		list[k].DevName = *deviceInfo.Name
 	}
 	return list, nil
 }
