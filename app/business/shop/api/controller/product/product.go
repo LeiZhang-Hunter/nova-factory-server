2026454ed665dac2b507466123183abd8eee51ef
zhanglei <357733652@qq.com>
2026-05-05 21:21:26 +0800

    fix: 修复 info

--- app/business/shop/api/controller/product/product.go
+++ app/business/shop/api/controller/product/product.go
@@ -7,35 +7,32 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Product 前台商品控制器。
 type Product struct {
 	service shopservice.IShopGoodsService
 }
 
-// NewProduct 创建前台商品控制器。
 func NewProduct(service shopservice.IShopGoodsService) *Product {
 	return &Product{service: service}
 }
 
 func (p *Product) PublicRoutes(router *gin.RouterGroup) {
 	group := router.Group("/api/v1/app/shop/product")
-	group.GET("/info/:id", p.GetByID)
+	group.GET("/info/:id", p.Info)
 }
 
 func (p *Product) PrivateRoutes(router *gin.RouterGroup) {
 
 }
 
-// GetByID 获取商品详情
-// @Summary 获取商品详情
-// @Description 根据ID获取商品详情
+// Info 读取商品详情
+// @Summary 读取商品详情
+// @Description 根据ID读取商品详情
 // @Tags app接口/商城/App商品
-// @Param id path int true "商品ID"
-// @Security BearerAuth
 // @Produce application/json
+// @Param id path int true "商品ID"
 // @Success 200 {object} response.ResponseData "获取成功"
 // @Router /api/v1/app/shop/product/info/{id} [get]
-func (p *Product) GetByID(c *gin.Context) {
+func (p *Product) Info(c *gin.Context) {
 	id := baizeContext.ParamInt64(c, "id")
 	if id == 0 {
 		baizeContext.ParameterError(c)
