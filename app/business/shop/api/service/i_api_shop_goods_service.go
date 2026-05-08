ba8f251cc4c6a5e84a4d1dee023b5dd8d1c50dee
zhanglei <35>
2026-05-08 13:21:27 +0800

    fix: 修复api 商品服务

--- app/business/shop/api/service/i_api_shop_goods_service.go
+++ app/business/shop/api/service/i_api_shop_goods_service.go
@@ -2,12 +2,13 @@ package service
 
 import (
 	"nova-factory-server/app/business/shop/api/models"
+	"nova-factory-server/app/business/shop/product/shopmodels"
 
 	"github.com/gin-gonic/gin"
 )
 
 // IApiShopGoodsService  商品服务接口
 type IApiShopGoodsService interface {
-	GetByID(c *gin.Context, id int64) (*models.Goods, error)
+	GetByID(c *gin.Context, id int64) (*shopmodels.Goods, error)
 	List(c *gin.Context, query *models.GoodsQuery) (*models.GoodsListData, error)
 }
